fccb1f55682c00f9c7c1f90d29c436478dc8edb5
wiseStann <servicename18@gmail.com>
2024-09-26 23:31:03 +0300

    Добавил mutex

--- internal/handlers/registration.go
+++ internal/handlers/registration.go
@@ -2,31 +2,35 @@ package handlers
 
 import (
 	"net/http"
+	"sync"
 	"time"
 
 	"youpin/internal/errors"
 	"youpin/internal/models"
 )
 
-var nextUserID uint64 = 2
+var (
+	nextUserID uint64 = 2
 
-var registeredUsers []models.User = []models.User{
-	{
-		UserID:       1,
-		UserName:     "admin",
-		NickName:     "admin",
-		Email:        "example@test.com",
-		Password:     "12345678",
-		BirthTime:    time.Date(2000, 1, 1, 0, 0, 0, 0, time.Now().Location()),
-		Gender:       "table",
-		AvatarUrl:    "",
-		Followers:    []models.User{},
-		Following:    []models.User{},
-		Boards:       []models.Board{},
-		CreationTime: time.Now(),
-		UpdateTime:   time.Now(),
-	},
-}
+	regUsrMutex                   = &sync.Mutex{}
+	registeredUsers []models.User = []models.User{
+		{
+			UserID:       1,
+			UserName:     "admin",
+			NickName:     "admin",
+			Email:        "example@test.com",
+			Password:     "12345678",
+			BirthTime:    time.Date(2000, 1, 1, 0, 0, 0, 0, time.Now().Location()),
+			Gender:       "table",
+			AvatarUrl:    "",
+			Followers:    []models.User{},
+			Following:    []models.User{},
+			Boards:       []models.Board{},
+			CreationTime: time.Now(),
+			UpdateTime:   time.Now(),
+		},
+	}
+)
 
 func userIsAlreadyRegistered(u models.User) error {
 	for _, user := range registeredUsers {
@@ -58,7 +62,9 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	regUsrMutex.Lock()
 	registeredUsers = append(registeredUsers, user)
+	regUsrMutex.Unlock()
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Registration successfull"))
