b4e42886d3aa5645175255ee159d7e594d5e3b2d
Ivan Ivashkin <f3shok@gmail.com>
2024-10-29 15:31:53 +0300

    Добавил миграцию

---
+++ db/migrations/000001_create_tables.down.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS board;
+DROP TABLE IF EXISTS pin;
+DROP TABLE IF EXISTS section;
+DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS bookmark;
+DROP TABLE IF EXISTS saved_pin_to_board;
+DROP TABLE IF EXISTS saved_pin_to_section;
+DROP TABLE IF EXISTS saved_board;
+DROP TABLE IF EXISTS follower;
